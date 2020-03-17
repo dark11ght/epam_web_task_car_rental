6fb6ca2a3b7154f06506c298cae9a68723d53f43
Pavel <pavel.elektrik@bk.ru>
2020-03-17 14:35:03 +0300

    Fix name field DB

---
+++ src/main/webapp/js/registration_page.js
@@ -0,0 +1,68 @@
+
+$(document).ready(function(){
+    var $submitBtn = $("#form input[type='submit']");
+    var $passwordBox = $("#password");
+    var $confirmBox = $("#confirm_password");
+    var $errorMsg =  $('<span id="error_msg">Passwords do not match.</span>');
+
+    // This is incase the user hits refresh - some browsers will maintain the disabled state of the button.
+    $submitBtn.removeAttr("disabled");
+
+    function checkMatchingPasswords(){
+        if($confirmBox.val() != "" && $passwordBox.val != ""){
+            if( $confirmBox.val() != $passwordBox.val() ){
+                $submitBtn.attr("disabled", "disabled");
+                $errorMsg.insertAfter($confirmBox);
+            }
+        }
+    }
+
+    function resetPasswordError(){
+        $submitBtn.removeAttr("disabled");
+        var $errorCont = $("#error_msg");
+        if($errorCont.length > 0){
+            $errorCont.remove();
+        }
+    }
+
+
+    $("#confirm_password, #password")
+        .on("keydown", function(e){
+            /* only check when the tab or enter keys are pressed
+             * to prevent the method from being called needlessly  */
+            if(e.keyCode == 13 || e.keyCode == 9) {
+                checkMatchingPasswords();
+            }
+        })
+        .on("blur", function(){
+            // also check when the element looses focus (clicks somewhere else)
+            checkMatchingPasswords();
+        })
+        .on("focus", function(){
+            // reset the error message when they go to make a change
+            resetPasswordError();
+        })
+
+});
+
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
